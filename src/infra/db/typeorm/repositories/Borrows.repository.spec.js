f3350da796238efd3291e31ec7135f18baecd6ed
Patrick Andrade <andrade.patrick@gmail.com>
2025-11-03 22:33:54 -0300

    70 - Criando teste de devolver

--- src/infra/db/typeorm/repositories/Borrows.repository.spec.js
+++ src/infra/db/typeorm/repositories/Borrows.repository.spec.js
@@ -58,4 +58,25 @@ describe('Emprestimo Repository Typeorm', () => {
 
         expect(devolver.date_return).toBe(borrow.date_return)
     })
+
+    test('Deve atualizar a data de delução no banco de dados corretamente ', async () =>{
+        const user = await typeormUserRepository.save(userDTO)
+        const book = await typeormBookRepository.save(bookDTO)
+
+        const borrow = await typeormBorrowRepository.save({
+            user_id: user.id,
+            book_id: book.id,
+            date_borrow: '2024-01-26',
+            date_return: '2024-01-26'
+        })
+
+        await sut.return({
+            borrow_id: borrow.id,
+            date_return: borrow.date_return
+        })
+
+        const getBorrowByID = await typeormBorrowRepository.findOneBy({borrow_id: borrow.id})
+
+        expect(getBorrowByID.date_return).toBe('2024-01-26')
+    })
 })
