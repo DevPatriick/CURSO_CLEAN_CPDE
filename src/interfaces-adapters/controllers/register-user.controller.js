75cdadac7f2f0dbd2d87df706168b1478fb6df53
Patrick Andrade <andrade.patrick@gmail.com>
2025-11-08 23:59:17 -0300

    Instalam zod para validação dos campos recebidos

--- src/interfaces-adapters/controllers/register-user.controller.js
+++ src/interfaces-adapters/controllers/register-user.controller.js
@@ -1,10 +1,32 @@
 const { AppError } = require("../../shared/errors");
 const httpResponse = require("../../shared/helpers/http.response")
 
+const {z} = require('zod')
+const zodValidator = z.object({
+    name: z.string({
+        required_error: 'Nome Completo é obrigatório'
+    }),
+    CPF: z.string({
+        required_error: 'CPf é obrigatório'
+    })
+    .refine((value) => /^([0-9]{3}\.?[0-9]{3}\.[0-9]{3}\-?[0-9]{2})$/.test(value)),
+    address: z.string({
+        required_error: 'Endereço é obrigatório'
+    }),
+    phone: z.string({
+        required_error: 'Telefone é obrigatório'
+    }),
+    email: z.string({
+        required_error: 'Email: é obrigatório'
+    }).email({
+        message: 'Email deve ser válido'
+    })
+})
+
 module.exports = registerUserController = async ({registerUserUseCase,httpRequest}) => {
     const checkDepency = !registerUserUseCase || !httpRequest || !httpRequest.body;
     if(checkDepency) throw new AppError(AppError.dependecy)
-    const {name, CPF, address, phone, email} = httpRequest.body
+    const {name, CPF, address, phone, email} = zodValidator.parse(httpRequest.body)
 
 
     const output = await registerUserUseCase({
