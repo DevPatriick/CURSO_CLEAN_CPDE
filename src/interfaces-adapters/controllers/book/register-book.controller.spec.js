1d2ec5a8f417b483e5e7bffa571adc7cdd43c646
Patrick Andrade <andrade.patrick@gmail.com>
2025-11-10 23:03:36 -0300

    Teste para erro 400, erro ao cadastrar livro

--- src/interfaces-adapters/controllers/book/register-book.controller.spec.js
+++ src/interfaces-adapters/controllers/book/register-book.controller.spec.js
@@ -5,10 +5,8 @@ const registerBookController = require("./register-book.controller")
 
 
 describe('Cadastrar livro controller', () => {
-    const registerBookUseCase = jest.fn()
-    it('Deve retornar um httpRequest 200 e null de sucesso ao cadastrar livro', async () => {
-        registerBookUseCase.mockResolvedValue(Either.Right(null))
-        const httpRequest = {
+    const httpReqDTO = () => {
+        return httpRequest = {
             body: {
                 name: 'nome_livro',
                 quantity: 1,
@@ -17,6 +15,12 @@ describe('Cadastrar livro controller', () => {
                 ISBN: 11122233344
             }
         }
+    }
+
+    const registerBookUseCase = jest.fn()
+    it('Deve retornar um httpRequest 200 e null de sucesso ao cadastrar livro', async () => {
+        registerBookUseCase.mockResolvedValue(Either.Right(null))
+        const httpRequest = httpReqDTO()
 
         const response = await registerBookController({
             registerBookUseCase,
@@ -24,6 +28,21 @@ describe('Cadastrar livro controller', () => {
         })
 
         expect(response).toEqual(httpResponse(201, null))
+        expect(registerBookUseCase).toHaveBeenCalledWith(httpRequest.body)
+        expect(registerBookUseCase).toHaveBeenCalledTimes(1)
+
+    })
+
+    it('Deve retornar um httpResponse 400 e error ao tentar cadastrar um livro', async () => {
+        registerBookUseCase.mockResolvedValue(Either.Left({message: 'logica_invalida'}))
+        const httpRequest = httpReqDTO()
+
+        const response = await registerBookController({
+            registerBookUseCase,
+            httpRequest
+        })
+
+        expect(response).toEqual(httpResponse(400, 'logica_invalida'))
         expect(registerBookUseCase).toHaveBeenCalledWith(httpRequest.body)
         expect(registerBookUseCase).toHaveBeenCalledTimes(1)
 
