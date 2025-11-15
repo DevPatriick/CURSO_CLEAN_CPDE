ac607337f73e18415227be8f99e083d6b7d207cf
Patrick Andrade <andrade.patrick@gmail.com>
2025-11-15 17:49:04 -0300

    Criação de teste de erro no controller de emprestar livro

--- src/interfaces-adapters/controllers/borrow-books/borrow-books.controller.spec.js
+++ src/interfaces-adapters/controllers/borrow-books/borrow-books.controller.spec.js
@@ -1,6 +1,7 @@
-const { Either } = require("../../../shared/errors")
+const { Either, AppError } = require("../../../shared/errors")
 const httpResponse = require("../../../shared/helpers/http.response")
 const borrowBooksController = require("./borrow-books.controller")
+const { ZodError } = require('zod')
 
 
 describe('Controller borrow books', () => {
@@ -25,6 +26,29 @@ describe('Controller borrow books', () => {
                 book_id: 123,
                 date_borrow: expect.any(Date),
                 date_return: expect.any(Date)
+            }
+        }
+    }
+
+
+    const httpRequesDatetDTO = () => {
+        return httpRequest = {
+            body: {
+                user_id: 123,
+                book_id: 123,
+                date_borrow: "2025-10-20",
+                date_return: "2025-10-13"
+            }
+        }
+    }
+
+    const responseUseCaseDateDTO = () => {
+        return httpRequest = {
+            body: {
+                user_id: 123,
+                book_id: 123,
+                date_borrow: new Date("2025-10-20"),
+                date_return: new Date("2025-10-13")
             }
         }
     }
@@ -62,6 +86,36 @@ describe('Controller borrow books', () => {
 
         expect(response).toEqual(httpResponse(400, 'usuario_com_livro_emprestado'))
         expect(borrowBooksUseCase).toHaveBeenCalledWith(httpReqReturnUseCase.body)
+        expect(borrowBooksUseCase).toHaveBeenCalledTimes(1)
+    })
+
+    it('Deve retornar um erro se o emprestar livro useCase não forem fornecidos', () => {
+        expect(() => borrowBooksController({})).rejects.toThrow(
+            new AppError(AppError.dependecy)
+        )
+    })
+
+    it('Deve retornar um erro do zodValidator', () => {
+        const httpRequest = {
+            body: {}
+        }
+
+        expect(() => borrowBooksController({borrowBooksUseCase, httpRequest})).rejects.toBeInstanceOf(ZodError)
+    })
+
+    it('Deve retornar um erro ao emprestar por que a data de retorno é menor que a data de retirada', async () => {
+        borrowBooksUseCase.mockResolvedValue(Either.Left({ message: 'invalid_date'}))
+        const httpRequest = httpRequesDatetDTO()
+
+        const response = await borrowBooksController({
+            borrowBooksUseCase,
+            httpRequest
+        })
+
+        const httpRequestDateDTO = responseUseCaseDateDTO()
+
+        expect(response).toEqual(httpResponse(400, 'invalid_date'))
+        expect(borrowBooksUseCase).toHaveBeenCalledWith(httpRequestDateDTO.body)
         expect(borrowBooksUseCase).toHaveBeenCalledTimes(1)
     })
 })
